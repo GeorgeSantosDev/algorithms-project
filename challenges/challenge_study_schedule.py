53674202ab5a02ec97e7f99d5f9fb9548dce47fc
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-25 16:07:23 -0300

    chore: Fix lint error

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,18 +1,26 @@
 def validate_data(entry, exit):
-    return isinstance(entry, str) or isinstance(exit, str) or entry == None or exit == None or entry > exit or entry < 0 or exit < 0
+    return (
+        isinstance(entry, str)
+        or isinstance(exit, str)
+        or not entry
+        or not exit
+        or entry > exit
+        or entry < 0
+        or exit < 0
+    )
 
 
 def study_schedule(permanence_period, target_time):
-    if target_time == None:
+    if not target_time:
         return None
-    
+
     counter = 0
-    
+
     for entry, exit in permanence_period:
         if validate_data(entry, exit):
             return None
-        
+
         if entry <= target_time and exit >= target_time:
             counter += 1
-    
+
     return counter
