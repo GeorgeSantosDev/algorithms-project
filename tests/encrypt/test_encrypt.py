ec22f666f8eabff89a1f9a78b77524f909427a5d
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-25 16:25:00 -0300

    test: Create test for encrypt_message function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Test", "Bad Call")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 5)
+
+    assert encrypt_message("protect", 10) == "tcetorp"
+    assert encrypt_message("protect", 2) == "tceto_rp"
+    assert encrypt_message("protect", 3) == "orp_tcet"
