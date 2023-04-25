c6a2285b4c903e60051272468a6a3e6f220e3413
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-25 15:59:59 -0300

    feat: Implement study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
+def validate_data(entry, exit):
+    return isinstance(entry, str) or isinstance(exit, str) or entry == None or exit == None or entry > exit or entry < 0 or exit < 0
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time == None:
+        return None
+    
+    counter = 0
+    
+    for entry, exit in permanence_period:
+        if validate_data(entry, exit):
+            return None
+        
+        if entry <= target_time and exit >= target_time:
+            counter += 1
+    
+    return counter
