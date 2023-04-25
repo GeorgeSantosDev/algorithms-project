27b7897fa49b63ddefc4ae8dd0de32d534f7e01c
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-25 17:53:24 -0300

    feat: Implement is_palindrome_recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        word == ""
+        or low_index == (len(word) // 2) + 1
+        or len(word) == 1
+        or word[low_index] != word[high_index]
+    ):
+
+        return (
+            True
+            if low_index == (len(word) // 2) + 1 or len(word) == 1
+            else False
+        )
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
