9be61614195df982298f38bfacc6e0858f7e2af5
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-26 13:00:41 -0300

    feat: Implement find_duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,26 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    numbers_count = dict()
+
+    if nums == None:
+        return False
+
+    for number in nums:
+        if isinstance(number, str) or number < 0:
+            return False
+        elif number in numbers_count:
+            numbers_count[number] += 1
+        else:
+            numbers_count[number] = 1
+
+    numbers_count_list = list(numbers_count.items())
+
+    duplicate_numbers = []
+
+    for numb, counter in numbers_count_list:
+        if counter > 1:
+            duplicate_numbers.append(numb)
+
+    if len(duplicate_numbers) == 1:
+        return duplicate_numbers[0]
+
+    return False
