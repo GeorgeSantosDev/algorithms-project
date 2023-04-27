735501a9845181cf60fe26c053af85a4fa8653d3
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-27 16:13:24 -0300

    feat: Implement is_palindrome_iterative function

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    length = len(word)
+
+    if word == "":
+        return False
+
+    if length == 1:
+        return True
+
+    for index in list(range(0, (length // 2) + 1)):
+        if not word[index] == word[length - 1 - index]:
+            return False
+
+    return True
